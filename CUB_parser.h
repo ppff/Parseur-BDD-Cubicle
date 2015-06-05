27bba0ff95268ff5085437246c3630f0f6d1131c
Florian Fontan <florian.fontan@ensimag.grenoble-inp.fr>
2015-06-05 10:32:17 +0200

    Update parser API

--- CUB_parser.h
+++ CUB_parser.h
@@ -1,28 +1,30 @@
 #ifndef CUB_PARSER_H
 #define CUB_PARSER_H
 
+/**
+ *
+ * In order to use the CUB_parser_parse_file function,
+ * you need to implement this header in CUB_parser_def.h:
+ *
+ * #include <stdint.h>
+ * #include <stdbool.h>
+ *
+ * bool CUB_parser_get_next_line(char buff[64], void *arg);
+ * void CUB_parser_list_add(void *list, uint32_t x, uint32_t y, uint32_t z);
+ *
+ *
+ * Usage example:
+ * list *l;
+ * list_init(l);
+ * FILE *f = fopen(filename);
+ * int status = CUB_parser_parse_file(list, f);
+ * fclose(file);
+ */
+
+
+#include "CUB_parser_def.h"
 #define MAX_SUPPORTED_VERSION 1
 
-#ifdef CUBICLE_SOFTWARE
-#define LIST_TYPE         ;
-#define FOPEN(FILENAME)   ;
-#define LIST_ADD(X, Y, Z) ;
-#define FCLOSE            ;
-
-#else
-#include <stdlib.h>
-#include <stdio.h>
-#include "applications/crystal_structure/CUB_LED_list.h"
-#include "fatfs.h"
-#define LIST_TYPE         CUB_LED_list_t *l
-#define FOPEN(FILENAME)   FIL *file = NULL;\
-                          f_open(file, FILENAME, FA_READ)
-#define READ_LINE(BUFF)   f_gets(BUFF, 64, file)
-#define LIST_ADD(X, Y, Z) CUB_LED_t led = {(X), (Y), (Z), NULL};\
-                          CUB_LED_list_add(l, &led)
-#define FCLOSE            f_close(file)
-#endif
-
-int CUB_parse_file(char *filename, LIST_TYPE);
+int CUB_parser_parse_file(void *CUB_parser_get_next_line_arg, void *list_add_arg);
 
 #endif
